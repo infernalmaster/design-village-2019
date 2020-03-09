acc9358bae232f48c184152ffa5d31e24f4a3131
Rostyslav Diachok <infernalmaster1@gmail.com>
2020-03-09 17:36:49 +0200

    fix cursor performance

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -3,35 +3,67 @@ import { css } from "@emotion/core"
 
 export class Cursor extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = { x: -10, y: 0, isFocused: false };
+    this.state = { x: -10, y: 0, isFocused: false }
   }
 
   componentDidMount() {
-    window.addEventListener('mousemove', this.handlerMouseMove)
+    window.addEventListener("mousemove", this.handlerMouseMove, true)
   }
 
   componentWillUnmount() {
-    window.removeEventListener('mousemove', this.handlerMouseMove)
+    window.removeEventListener("mousemove", this.handlerMouseMove, true)
   }
 
-  handlerMouseMove = (e) => {
-    let isFocused = ['A', 'BUTTON'].includes(e.target.tagName)
-                 || ['A', 'BUTTON'].includes(e.target.parentNode.tagName)
+  handlerMouseMove = e => {
+    let isFocused =
+      ["A", "BUTTON"].includes(e.target.tagName) ||
+      ["A", "BUTTON"].includes(e.target.parentNode.tagName)
     this.setState({ x: e.clientX, y: e.clientY, isFocused })
   }
 
   render() {
-    const { x, y, isFocused } = this.state;
+    const { x, y, isFocused } = this.state
+
     return (
       <div
-        className={`cursor ${isFocused ? 'cursor--is-focused' : ''}`}
         css={css`
-          top: ${y}px;
-          left: ${x}px;
+          position: fixed;
+          z-index: 9999;
+          width: 0;
+          height: 0;
+          pointer-events: none;
+          will-change: transform;
+
+          @media only screen and (max-width: 1024px) {
+            display: none;
+          }
         `}
+        style={{
+          transform: `translate(${x}px, ${y}px)`,
+        }}
       >
+        <div
+          css={css`
+            position: absolute;
+            width: 80px;
+            height: 80px;
+            background: #fcfb63;
+            border-radius: 50%;
+
+            pointer-events: none;
+            transform-origin: center;
+            transition: all 0.1s;
+            will-change: transform;
+          `}
+          style={{
+            transform: isFocused
+              ? "translate(-50%, -50%) scale(1)"
+              : "translate(-50%, -50%) scale(0.125)",
+            opacity: isFocused ? 0.2 : 1,
+          }}
+        />
       </div>
     )
   }
